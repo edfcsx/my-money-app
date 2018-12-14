7a039f1fb124872371d4fba2dd1a08262a4601f8
Eduardo Cipriano <edfcsx@gmail.com>
2018-12-14 06:44:55 -0300

    insert routes , footer and menu itens

--- frontend/src/commons/template/menu.jsx
+++ frontend/src/commons/template/menu.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import MenuItem from './menuItem'
+import MenuTree from './menuTree'
 
 export default props => (
     <ul className='sidebar-menu'>
-        
+        <MenuItem path='#' label='Dashboard' icon='dashboard'/>
+        <MenuItem path='#billingCycles' label='Ciclos de Pagamento' icon='usd'/>
+        <MenuTree label='cadastro' icon='edit'>
+            <MenuItem path='#billingCycles' label='Dashboard' icon='dashboard'/>
+        </MenuTree>
     </ul>
 )
--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -1,12 +1,18 @@
 import '../commons/template/dependencies'
 import React from 'react'
+import Routes from './routes'
 
 import Header from '../commons/template/header'
 import Sidebar from '../commons/template/sidebar'
+import Footer from '../commons/template/footer'
 
 export default props => (
     <div className='wrapper'>
         <Header/>
         <Sidebar/>
+            <div className='content-wrapper'>
+                <Routes/>
+            </div>
+        <Footer/>
     </div>
 )
